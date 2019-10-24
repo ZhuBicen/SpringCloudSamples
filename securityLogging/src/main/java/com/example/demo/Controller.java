8b7f14c7e7d8393bd3f67abe58de9deafe37d41a
bicen.zhu <bicen.zhu@nokia-sbell.com>
2019-10-24 16:33:55 +0800

    add security logging

--- securityLogging/src/main/java/com/example/demo/Controller.java
+++ securityLogging/src/main/java/com/example/demo/Controller.java
@@ -1,4 +1,19 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+    @GetMapping("/admin/hello")
+    String adminHello() {
+        return "Hello admin";
+    }
+
+
+    @GetMapping("/user/hello")
+    String userHello() {
+        return "Hello user";
+    }
 }
